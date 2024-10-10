fea939eb03d4d2aaff34ae4f1d367d7210e095a1
Gourav Parmar <156908130+gouravparmar17@users.noreply.github.com>
2024-10-10 21:29:08 +0530

    Add files via upload

---
+++ prog1.py
@@ -0,0 +1,17 @@
+# Python program to demonstrate using the input() and print() functions. The
+# program should:
+# a. Prompt the user to input two values, a and b.
+# b. Display the result of applying the + operator to a and b without any type casting
+# (showing string concatenation behavior).
+# c. Prompt the user to input two values, a and b as integers, then apply the +
+# operator again to demonstrate the behavior of integer addition.
+
+
+a = input("Enter any number : ")
+b = input("Enter any number : ")
+print(a + b)
+
+
+a = int(input("Enter any number : "))
+b = int(input("Enter any number : "))
+print(a + b)
