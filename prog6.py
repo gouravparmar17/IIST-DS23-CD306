4cee9cdbc5457278563b341c6ff511298ec8d73f
Gourav Parmar <156908130+gouravparmar17@users.noreply.github.com>
2024-10-12 17:21:38 +0530

    Add files via upload

---
+++ prog6.py
@@ -0,0 +1,15 @@
+n=int(input("Enter the number = "))
+# if n%10==0:
+#     print(n,"is divisible by 10")
+# if n%11==0:
+#     print(n,"is divisible by 11")
+# if n%10!=0 and n%11!=0:
+#     print(n,"is not divisible by 10 and 11")
+
+
+if n%10==0:
+    print(n,"is divisible by 10")
+if n%11==0:
+    print(n,"is divisible by 11")
+elif n%10!=0:
+    print(n,"is not divisible by 10 and 11")
