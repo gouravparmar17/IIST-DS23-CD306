64b3333316b5a73f3f323647cf96ae42a9116fe5
Gourav Parmar <156908130+gouravparmar17@users.noreply.github.com>
2024-11-18 20:44:29 +0530

    prog6.py

--- prog6.py
+++ prog6.py
@@ -1,10 +1,10 @@
 n=int(input("Enter the number = "))
-# if n%10==0:
-#     print(n,"is divisible by 10")
-# if n%11==0:
-#     print(n,"is divisible by 11")
-# if n%10!=0 and n%11!=0:
-#     print(n,"is not divisible by 10 and 11")
+ if n%10==0:
+     print(n,"is divisible by 10")
+ if n%11==0:
+     print(n,"is divisible by 11")
+ if n%10!=0 and n%11!=0:
+     print(n,"is not divisible by 10 and 11")
 
 
 if n%10==0:
